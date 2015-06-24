196315fb7076f2025fce0ba10c4631a5430e5d2a
Garth Poitras <garth22@gmail.com>
2015-06-24 16:10:51 -0400

    get things working

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
-/* jshint node: true */
-'use strict';
+var CssnextCompiler = require('broccoli-cssnext-single');
+var path            = require('path');
+var merge           = require('lodash.merge');
+var mergeTrees      = require('broccoli-merge-trees');
+var checker         = require('ember-cli-version-checker');
 
-var cssnext = require('broccoli-cssnext');
-var Funnel = require('broccoli-funnel');
-
-function Preprocessor(options) {
+function CssnextPlugin(optionsFn) {
   this.name = 'ember-cli-cssnext';
-  this.options = options || {};
+  this.ext = 'css';
+  this.optionsFn = optionsFn;
 }
 
-Preprocessor.prototype.toTree = function(tree, inputPath, outputPath, inputOptions) {
-  return new Funnel(cssnext(tree, this.options), {
-    srcDir: inputPath,
-    destDir: outputPath
+CssnextPlugin.prototype.toTree = function(tree, inputPath, outputPath, inputOptions) {
+  var options = merge({}, this.optionsFn(), inputOptions);
+
+  var ext = this.ext;
+  var paths = options.outputPaths || {
+    app: options.registry.app.options.outputPaths.app.css
+  };
+
+  var trees = Object.keys(paths).map(function(file) {
+    var input = path.join(inputPath, file + '.' + ext);
+    var output = paths[file];
+
+    return new CssnextCompiler([tree], input, output, options);
   });
+
+  return mergeTrees(trees);
 };
 
 module.exports = {
-  name: 'ember-cli-cssnext',
+  name: 'Ember CLI Cssnext',
+  project: this.project,
+
+  shouldSetupRegistryInIncluded: function() {
+    return !checker.isAbove(this, '0.2.0');
+  },
+
+  cssnextOptions: function() {
+    var env = process.env.EMBER_ENV;
+    var app = this.app;
+    var options = (app && app.options && app.options.cssnextOptions) || {};
+
+    if ((options.sourceMap === undefined) && (env === 'development')) {
+      options.sourceMap = true;
+    }
+
+    return options;
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
+    registry.add('css', new CssnextPlugin(this.cssnextOptions.bind(this)));
+
+    // prevent conflict with broccoli-cssnext-single if it's installed
+    if (registry.remove) {
+      registry.remove('css', 'broccoli-cssnext-single');
+    }
+  },
 
   included: function(app) {
+    this.app = app; // used to provide back-compat for ember-cli < 0.2.0 in cssnextOptions()
     this._super.included.apply(this, arguments);
-    app.registry.add('css', new Preprocessor(app.options && app.options.cssnextOptions));
+
+    if (this.shouldSetupRegistryInIncluded()) {
+      this.setupPreprocessorRegistry('parent', app.registry);
+    }
+  },
+
+  buildError: function(error) {
+    if (error) {
+      error.stack = error.stack || JSON.stringify(error, null, 2);
+    }
   }
-};
+}
